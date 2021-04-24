b4944388284865798adf358c3a4d35c8d3ca6f38
ThomasCazzola <info.chat.now@gmail.com>
2021-04-24 23:54:41 +0200

    Create Script.js

---
+++ Docs/Attività PCTO/JS/Script.js
@@ -0,0 +1,19 @@
+function Apri1(){
+    document.getElementById("primo").hidden = !document.getElementById("primo").hidden;
+}
+
+function Apri2(){
+    document.getElementById("secondo").hidden = !document.getElementById("secondo").hidden;
+}
+
+function Apri3(){
+    document.getElementById("terzo").hidden = !document.getElementById("terzo").hidden;
+}
+
+function Apri4(){
+    document.getElementById("quarto").hidden = !document.getElementById("quarto").hidden;
+}
+
+function Apri5(){
+    document.getElementById("quinto").hidden = !document.getElementById("quinto").hidden;
+}
